c6cd2ff9b66ae55dc5f3211cd3f6a1c29e167353
Luis Rodríguez Camas <luis.rodriguez02@globant.com>
2026-02-11 11:26:46 +0100

    feat: update task list structure

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const formBlock = document.getElementById("form-block");
 const btnToggleForm = document.getElementById("btn-toggle-form");
 
+const listItems = document.querySelectorAll('#task-list li');
+console.log(listItems)
+
 btnToggleForm.addEventListener("click", toggleForm);
 
 function toggleForm() {
