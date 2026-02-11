407c7a441b39ecef75fdf3cf67aa6f9a9b63b0bc
Luis Rodríguez Camas <luis.rodriguez02@globant.com>
2026-02-11 10:10:17 +0100

    feat: add form toggle behaviour

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log("Works Fine")
+const formBlock = document.getElementById("form-block");
+const btnToggleForm = document.getElementById("btn-toggle-form");
+
+btnToggleForm.addEventListener("click", toggleForm);
+
+function toggleForm() {
+  console.log(formBlock.classList);
+  // Toggles the specified class
+  formBlock.classList.toggle("hidden");
+}
