7084466ba525a21e8cce93c8980b1b549d7b9c78
Luis Rodríguez Camas <luis.rodriguez02@globant.com>
2026-02-12 18:15:24 +0100

    feat: maintain tasks using localstorage

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ const taskArray = [];
 const taskForm = document.querySelector("#form-task");
 const btnToggleForm = document.getElementById("btn-toggle-form");
 
+buildTaskListFromLocalStorage();
+
 // EVENTS
 btnToggleForm.addEventListener("click", toggleForm);
 taskForm.addEventListener("submit", submitTask);
@@ -16,11 +18,13 @@ taskForm.addEventListener("submit", submitTask);
 // METHODS
 function toggleForm() {
   const formBlock = document.querySelector("#form-section");
+  // Returns a DOMTokenList with the classes.
   console.log(formBlock.classList);
   // Toggles the specified class
   formBlock.classList.toggle("hidden");
 }
 
+// TODO -- Might be a good idea to pop out the const to get DOM elements
 function submitTask(event) {
   // Prevents the page to be refreshed
   event.preventDefault();
@@ -40,12 +44,37 @@ function submitTask(event) {
     taskList.appendChild(taskHTMLElement);
 
     taskArray.push(newTaskObj);
-    console.log(taskArray);
+
+    const stringifiedObj = JSON.stringify(taskArray);
+    localStorage.setItem("taskList", stringifiedObj);
+    // const newTaskArray = JSON.parse(stringifiedObj)
 
     // Cleans the form
     taskForm.reset();
   } else {
     alert("Task name must have at least 3 characters");
-    formInputName.style.backgroundColor = "red";
+    formInputName.style.backgroundColor = "#fff6f6";
+    formInputName.style.border = "2px solid red";
+  }
+}
+
+function buildTaskListFromLocalStorage() {
+  const retrievedList = JSON.parse(localStorage.getItem("taskList"));
+  if (retrievedList != null) {
+    console.log("Hay cosas en el localStorage!");
+
+    retrievedList.forEach((item) => {
+      const storedTask = new TaskPrototype(item.name, item.description);
+
+      taskArray.push(storedTask);
+
+      const taskHTMLElement = storedTask.toHTMLElement();
+
+      const taskList = document.querySelector("#task-list");
+      taskList.appendChild(taskHTMLElement);
+      console.log(taskArray);
+    });
+  } else {
+    console.log("No hay nada en el localStorage!");
   }
 }
