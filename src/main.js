347ed5785cf001f0e4d81dafb125581435055a3f
Luis Rodríguez Camas <luis.rodriguez02@globant.com>
2026-02-13 10:43:21 +0100

    refactor: code refactoring

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// ! If we don't put the file extension it might give the error:
+// ! If we don't put the file extension (.js) it might give the error:
 // ! net::ERR_ABORTED 404 (Not Found)
 import { TaskPrototype } from "./model/TaskPrototype.js";
 
@@ -6,10 +6,21 @@ const regex = /^.{3,}$/;
 
 const taskArray = [];
 
+// DOM SELECTORS
 const taskForm = document.querySelector("#form-task");
-const btnToggleForm = document.getElementById("btn-toggle-form");
+const btnToggleForm = document.querySelector("#btn-toggle-form");
 
-buildTaskListFromLocalStorage();
+const formBlock = document.querySelector("#form-section");
+
+//submitTask Selectors
+const formInputName = document.querySelector("#form-task__input-name");
+const formInputDescription = document.querySelector("#form-task__input-desc");
+const errorMsg = document.querySelector("#name-error-msg");
+
+const taskList = document.querySelector("#task-list");
+// --------------------------------------------------------------------------//
+
+buildTasksListFromLocalStorage();
 
 // EVENTS
 btnToggleForm.addEventListener("click", toggleForm);
@@ -17,22 +28,25 @@ taskForm.addEventListener("submit", submitTask);
 
 // METHODS
 function toggleForm() {
-  const formBlock = document.querySelector("#form-section");
   // Returns a DOMTokenList with the classes.
   console.log(formBlock.classList);
   // Toggles the specified class
   formBlock.classList.toggle("hidden");
+  switchToggleFormMsg();
 }
 
-// TODO -- Might be a good idea to pop out the const to get DOM elements
+function switchToggleFormMsg() {
+  if (!formBlock.classList.contains("hidden")) {
+    console.log("DJASKDJAKDJA");
+    btnToggleForm.textContent = "Close Tab ❌";
+  } else {
+    btnToggleForm.textContent = "Add Task 💡";
+  }
+}
+
 function submitTask(event) {
   // Prevents the page to be refreshed
   event.preventDefault();
-
-  const formInputName = document.querySelector("#form-task__input-name");
-  const formInputDescription = document.querySelector("#form-task__input-desc");
-
-  const errorMsg = document.querySelector("#name-error-msg");
 
   const taskName = formInputName.value;
   const taskDescription = formInputDescription.value;
@@ -45,7 +59,6 @@ function submitTask(event) {
     const newTaskObj = new TaskPrototype(taskName, taskDescription);
     const taskHTMLElement = newTaskObj.toHTMLElement();
 
-    const taskList = document.querySelector("#task-list");
     taskList.appendChild(taskHTMLElement);
 
     taskArray.push(newTaskObj);
@@ -59,11 +72,12 @@ function submitTask(event) {
   } else {
     formInputName.style.backgroundColor = "#fff6f6";
     formInputName.style.border = "2px solid red";
-    errorMsg.textContent = " Task name must have at least 3 characters";
+    const errorMsgText = "Task's name must have at least 3 characters!";
+    errorMsg.textContent = errorMsgText;
   }
 }
 
-function buildTaskListFromLocalStorage() {
+function buildTasksListFromLocalStorage() {
   const retrievedList = JSON.parse(localStorage.getItem("taskList"));
   if (retrievedList != null) {
     console.log("Hay cosas en el localStorage!");
@@ -75,7 +89,6 @@ function buildTaskListFromLocalStorage() {
 
       const taskHTMLElement = storedTask.toHTMLElement();
 
-      const taskList = document.querySelector("#task-list");
       taskList.appendChild(taskHTMLElement);
       console.log(taskArray);
     });
